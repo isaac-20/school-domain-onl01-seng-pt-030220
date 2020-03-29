4d4ae55a4be8e1918c8837e59efdd5c3c1e7b4d1
Isaac Smith <isaacsmith215@aol.com>
2020-03-29 04:02:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,4 +3,17 @@ class School
     @name = name
     @roster = {}
   end
+  
+  def roster
+    @roster
+  end 
+  
+  def add_student(name, grade)
+    if @roster[grade] != nil 
+      @roster[grade << name]
+    else 
+      @roster[grade] = [name]
+    end 
+    
+  end
 end
